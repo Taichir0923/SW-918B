0e7d6c2ed56caa4eb52010634e56e3589cd9caf4
Taichir0923 <crystal19950923@gmail.com>
2021-11-20 10:11:10 +0800

    scroll reveal

---
+++ revise/static/js/script.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal('#zuun', {
+    origin: 'left',
+    distance: '200px'
+})
+
+ScrollReveal().reveal('#baruun', {
+    origin: 'right',
+    distance: '200px'
+})
+
+ScrollReveal().reveal('.hero', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1500
+})
+
+ScrollReveal().reveal('#overlay', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1200,
+    delay: 700
+})
