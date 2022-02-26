7b9103b4f38c0d568e3e6ea6348726ea22701414
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-26 11:09:14 +0800

    2.26

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -43,16 +43,59 @@
 
 // Гэрт олон зураг оруулаад, html Дотор харуул
 
-const heading = document.querySelector('#title');
+// const heading = document.querySelector('#title');
 
-function mouseEnter () {
-    heading.style.color = "red";
-}
+// function mouseEnter () {
+//     heading.style.color = "red";
+// }
 
-function mouseLeave () {
-    heading.style.color = "black";
-}
+// function mouseLeave () {
+//     heading.style.color = "black";
+// }
 
 // createElement
 
 // const imgTag = document.createElement('img');
+
+// create element
+const container = document.querySelector('.container');
+const files = document.querySelector('#files');
+
+
+// innerHTML - text, html tag
+// headingTg.innerHTML = `<span>
+//     <p>this is string data...</p>
+// </span>`
+
+// innerText - text
+// headingTg.textContent = "<h1>blabla</h1>"
+// // texContent - text
+// container.append(headingTg);
+
+function uploadFiles(){
+    const images = files.files;
+    for(var i = 0; i < images.length; i++){
+        const img = document.createElement('img');
+        img.setAttribute('class' , 'img blabla');
+        img.setAttribute('id' , 'img');
+        img.src = URL.createObjectURL(images[i]);
+        container.append(img);
+    }
+}
+
+files.addEventListener("change" , uploadFiles)
+
+document.addEventListener('click' , function(event){
+    if(event.target.id === "img"){
+        event.target.classList.toggle('border');
+        console.log(event.target.src);
+    }
+})
+
+// onclick
+
+// DOM
+// AsyncJS - Promise
+// Fetch API , JSON , localStorage
+// canvas - webGL
+
