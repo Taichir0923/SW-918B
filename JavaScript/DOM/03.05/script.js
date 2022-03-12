4ee2e670bfefa0c744a02a87aa6e9a802c76da41
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-03-12 10:44:19 +0800

    update

--- JavaScript/DOM/03.05/script.js
+++ JavaScript/DOM/03.05/script.js
@@ -8,15 +8,17 @@ const firstname = document.querySelector('#firstname');
 const lastname = document.querySelector('#lastname');
 const number = document.querySelector('#number');
 const password = document.querySelector('#password');
+const email = document.querySelector('#email');
 
 let users;
 init();
 class User {
-    constructor(firstname , lastname , number , password){
+    constructor(firstname , lastname , number , password , email){
         this.firstname = firstname;
         this.lastname = lastname;
         this.number = number;
         this.password = password;
+        this.email = email;
         this.id = Math.random().toString().split('.')[1]; 
     };
 };
@@ -32,8 +34,8 @@ function init(){
 
 btn.addEventListener('click' , function(event){
     event.preventDefault();
-    if(firstname.value.trim() !== '' && lastname.value.trim() !== '' && number.value.trim() !== '' && password.value.trim() !== '') {
-        const user = new User(firstname.value , lastname.value , number.value , password.value);
+    if(firstname.value.trim() !== '' && lastname.value.trim() !== '' && number.value.trim() !== '' && password.value.trim() !== '' && email.value.trim() !== '') {
+        const user = new User(firstname.value , lastname.value , number.value , password.value , email.value);
         users.push(user);
         clearForm();
         localStorage.setItem("users" , JSON.stringify(users));
@@ -48,6 +50,7 @@ function clearForm(){
     lastname.value = '';
     number.value = '';
     password.value = '';
+    email.value = '';
 }
 
 // хэрэглэгч бүртгэх форм хий...
@@ -66,3 +69,5 @@ function clearForm(){
 // }
 
 // JS object notation
+
+// github.com/taichir0923/sw-918b
