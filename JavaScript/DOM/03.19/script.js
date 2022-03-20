c77614e779242372b06fe277838cf6b5a3115b00
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-03-20 11:46:24 +0800

    API

--- JavaScript/DOM/03.19/script.js
+++ JavaScript/DOM/03.19/script.js
@@ -43,11 +43,11 @@ function testPromise(nas){
 // try catch
 async function getResult(){
     try {
-        const response = await fetch('https://randomuser.me/api/?results=50');
-        const {results} = await response.json();
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const results = await response.json();
         results.forEach((user , index) => {
             document.body.insertAdjacentHTML('beforeend' , `
-                <h1>${index + 1}. ${user.name.title} ${user.name.first} ${user.name.last}</h1>
+                <h1 class="getPosts" id="user-${user.id}">${index + 1}. ${user.name}</h1>
             `)
         })
     } catch (err) {
@@ -57,4 +57,55 @@ async function getResult(){
 // console.log('hi')
 
 // https://jsonplaceholder.typicode.com/
-// https://jsonplaceholder.typicode.com/posts?userId=5
+// https://jsonplaceholder.typicode.com/posts?userId=1
+
+// request
+// get - serverees data avah...
+// post - hereglegchees data avah
+// put - datag oor shine datagaar solih
+// patch - datanii todorhoi hesgiig oorchlohod ashiglana
+// delete - data ustgah huselt
+
+function postReq(){
+    const post = {
+        userId: 1,
+        title: "new post",
+        body: "body of post"
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts' , {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(post)
+    })
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.log(err.message));
+}
+
+async function testAxios(id){
+    console.log('loading...')
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts?userId=' + id);
+    if(data){
+        console.log(data);
+        console.log('Done!');
+    }
+}
+
+document.addEventListener('click' , e => {
+    if(e.target.classList.contains('getPosts')){
+        const id = e.target.id.split('-')[1];
+        testAxios(id);
+    }
+});
+
+// TypeScript - static typed - Microsoft 2016
+// JavaScript - dynamic typed
+
+// CSS - SASS
+
+// nodejs
+// 
+
+// 
