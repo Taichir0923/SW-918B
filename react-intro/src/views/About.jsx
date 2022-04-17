deecd44afd48c29b15efc841f65b6ba7cbf053c7
narada0923 <narada4027@gmail.com>
2022-04-17 11:57:15 +0800

    Update About.jsx

--- react-intro/src/views/About.jsx
+++ react-intro/src/views/About.jsx
@@ -1,8 +1,13 @@
-import {Fragment , useEffect} from 'react';
+import {Fragment , useEffect , useState} from 'react';
 import useFetch from '../hooks/useFetch';
 import Header from "../components/Menu";
+import { useLocation } from 'react-router-dom';
 const About = () => {
-    const {data} = useFetch('https://jsonplaceholder.typicode.com/users');
+    const {search} = useLocation();
+    const id = new URLSearchParams(search).get('user');
+    
+    const {data} = useFetch('https://jsonplaceholder.typicode.com/users/' + id);
+
 
     useEffect(() => {
         console.log(data)
@@ -14,3 +19,5 @@ const About = () => {
 
 export default About;
 // React fragment
+
+// posts => (ng shirheg post (comment)) , albums => (ng shirheg album (zurag)) , todos
